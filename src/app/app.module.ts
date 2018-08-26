cca32cf70da5403d1fa37f2580eff0f6cbe1b428
Yijun <wangyj2510@gmail.com>
2018-08-26 11:01:06 -0700

    second demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Student } from '../../shared/student.model';
+
+@Component({
+  // select: '[app-server]',
+  selector: 'app-server',
+  templateUrl: './server.component.html', // you can put real html here
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+    serverId = 10;
+    serverStatus = 'offLine';
+    counter = 0;
+    isLogin = false;
+    userName: string;
+    students: Student[] = [new Student('Bill Gates', 'Computer Science'),
+                           new Student('Steve Jobs', 'Computer Science'),
+                           new Student('Elon Musk', 'Computer Science')];
+
+    getServerStatus() {
+      return this.serverStatus;
+    }
+
+    counterPlus() {
+     this.counter ++;
+    }
+
+    resetCounter() {
+      this.counter = 0;
+    }
+
+    login() {
+      this.isLogin = true;
+    }
+
+    signOut() {
+      this.isLogin = false;
+    }
+
+    // Event Binding
+    onUpdateUserName(event: Event) {
+      this.userName = (<HTMLInputElement>event.target).value;
+    }
+}
