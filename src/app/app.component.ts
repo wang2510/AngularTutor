afb8bdd50a08eee6f2a166fa22beff21dd57a8ff
Yijun <wangyj2510@gmail.com>
2018-08-26 10:11:29 -0700

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '九章全栈Angular课程';
+  changeableName = '改我的名字吧';
+}
