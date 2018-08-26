771527fad71ab094573f7c75d9298a561a16f354
Yijun <wangyj2510@gmail.com>
2018-08-26 15:00:11 -0700

    finished

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   // select: '[app-server]',
@@ -6,7 +6,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server.component.html', // you can put real html here
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit {
-  constructor() {}
-  ngOnInit() { }
+export class ServerComponent {
+    serverId = 10;
+    serverStatus = 'offLine';
+    counter = 0;
+    isLogin = false;
+    userName: string;
+
+    getServerStatus() {
+      return this.serverStatus;
+    }
+
+    counterPlus() {
+     this.counter ++;
+    }
+
+    resetCounter() {
+      this.counter = 0;
+    }
+
+    login() {
+      this.isLogin = true;
+    }
+
+    signOut() {
+      this.isLogin = false;
+    }
+
+    // Event Binding
+    onUpdateUserName(event: Event) {
+      this.userName = (<HTMLInputElement>event.target).value;
+    }
 }
