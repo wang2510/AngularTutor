5e4a7f7862ead256f1b6264d8613820e956e8889
Yijun <wangyj2510@gmail.com>
2018-08-26 11:01:06 -0700

    fist demo flex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '九章全栈基础课!';
+  title = 'jiuzhangAngular';
+  changeableName = 'myName';
 }
