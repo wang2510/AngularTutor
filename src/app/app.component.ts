0e1821f818ef8e620907254c10e8b567859ab40e
Yijun <wangyj2510@gmail.com>
2020-01-04 11:07:24 -0800

    copyright

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jiuzhangAngular';
+  title = 'Welcome to Angular';
   changeableName = 'myName';
 }
